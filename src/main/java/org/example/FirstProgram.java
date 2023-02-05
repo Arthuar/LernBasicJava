f1df840d0f79b4d577b61f23fad364cb29c8a708
Arthuar <artur.dikun@gmail.com>
2023-02-05 12:38:59 +0200

    HelloJava

---
+++ src/main/java/org/example/FirstProgram.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("HelloJava");
+    }
+}
